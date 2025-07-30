31d25c7ef97da576da14528f598f0bd3e6a1c97c
Akhyar <akhyar1@mhs.unsyiah.ac.id>
2025-07-30 20:44:36 +0700

    add lottiefile

--- app/Pages/Contact.tsx
+++ app/Pages/Contact.tsx
@@ -1,3 +1,10 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import React from "react";
+
 export default function Contact() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <DotLottieReact src="/assets/icons/star.svg" loop autoplay />
+    </div>
+  );
 }
